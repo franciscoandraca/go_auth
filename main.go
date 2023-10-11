ac7f507a0f28e6a7412203a715a41cac8faadca1
franciscoandraca <francisco.andraca14@gmail.com>
2023-10-10 21:55:01 -0600

    Instalamos el ORM que se llama GORM y creamos la conexion a la base de datos MySql

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	dsn := "usuaeio:Contraseña@/base_dattos?parseTime=false"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		// Manejar el error
+		panic("Error: no se pudo conectar a la base de datos")
+	} else {
+		// La conexión a la base de datos MySQL se ha establecido con éxito
+		fmt.Println("La conexión a la base de datos MySQL se ha establecido con éxito")
+		fmt.Println(db)
+	}
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Get("/", home)
 
-	app.Listen(":3000")
+	app.Listen(":8000")
+}
+
+func home(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
 }
