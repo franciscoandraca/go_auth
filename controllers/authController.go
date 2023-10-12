468907efe116d037d97a53b10bd130f7d0d814cb
franciscoandraca <francisco.andraca14@gmail.com>
2023-10-11 21:42:19 -0600

    Creamos un directtorio Models dentro definicmos la estructtura de usuario y sus propiedades, creamos la funcion registro con el metodo post y regresamos la respuesta en JSON

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,9 +1,19 @@
 package controllers
 
 import (
+	"go_auth/models"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func Home(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+func Register(c *fiber.Ctx) error {
+
+	var user models.User
+
+	user.FirstName = "Francisco"
+	user.LastName = "Andraca"
+	user.Email = "email@gmail.com"
+	user.Password = "secret"
+
+	return c.JSON(user)
 }
