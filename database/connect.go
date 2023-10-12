7359147cd8110fa4cbe7248e357b2f13e6835070
franciscoandraca <francisco.andraca14@gmail.com>
2023-10-11 21:57:52 -0600

    Creamos nuestra primer modelo y automigracion

--- database/connect.go
+++ database/connect.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"go_auth/models"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +12,7 @@ func Conectar() {
 
 	dsn := "usuaeio:Contraseña@/base_dattos?parseTime=false"
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	conexion, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		// Manejar el error
@@ -20,5 +21,7 @@ func Conectar() {
 		// La conexión a la base de datos MySQL se ha establecido con éxito
 		fmt.Println("La conexión a la base de datos MySQL se ha establecido con éxito")
 	}
+
+	conexion.AutoMigrate(&models.User{})
 
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type User struct {
+	gorm.Model
 	Id        uint
 	FirstName string
 	LastName  string
-	Email     string
+	Email     string `gorm:"unique"`
 	Password  string
 }
